7bd0612496aedf5a9fff7f7d817bbeb9420f1433
gravity-zero <gravity.neo@gmail.com>
2022-02-05 02:39:25 +0100

    better logical append numpy array in new npy file for new adding reference_image

--- recognize_face/prepare_embedding.py
+++ recognize_face/prepare_embedding.py
@@ -2,11 +2,18 @@ import face_recognition
 import numpy as np
 import os
 import glob
+from npy_append_array import NpyAppendArray
 
 dir_identites="../our_faces/"
 
 face_encodings=[]
 face_names=[]
+is_npy_file = os.path.isfile('face_names.npy')
+
+if is_npy_file:
+    npyFaceName = np.load('face_names.npy')
+    npyFaceEncode = np.load('face_encodings.npy')
+
 
 id=1
 for dir_identite in os.listdir(dir_identites):
@@ -20,11 +27,29 @@ for dir_identite in os.listdir(dir_identites):
         print("Repertoire vide", dir_identite)
         continue
     for fichier in fichiers:
-        image=face_recognition.load_image_file(fichier)
-        embedding=face_recognition.face_encodings(image)[0]
-        face_encodings.append(embedding)
-        face_names.append(dir_identite.replace('_', ' '))
-            
-np.save("face_encodings", np.array(face_encodings))
-print(embedding)
-np.save("face_names", np.array(face_names))
+        filename = dir_identite.replace('_', ' ')
+        if is_npy_file and filename not in npyFaceName or not is_npy_file:
+            image=face_recognition.load_image_file(fichier)
+            embedding=face_recognition.face_encodings(image)[0]
+            face_encodings.append(embedding)
+            face_names.append(filename)
+
+if is_npy_file and len(face_names) > 0 :
+    for faceEncoding in face_encodings:            
+        np.save("face_encodings", np.array(np.append(npyFaceEncode, faceEncoding)))
+        print('Append encoding new face')
+    print(face_names)
+    for faceName in face_names: 
+        np.save("face_names", np.array(np.append(npyFaceName, faceName)))
+        print('Append new face', faceName, '\n')
+    print('New array', np.load('face_names.npy'))
+         
+elif len(face_names) > 0 :
+    np.save("face_encodings", np.array(face_encodings))
+    print('encoding all faces', embedding)
+    np.save("face_names", np.array(face_names))
+    print('Add all face names', face_names) 
+
+else:
+    print("Total ID's:", id)
+    print('Nothing to add')
