6d163dc64dad4c8269d797c33570f8d1afad4186
gravity-zero <gravity.neo@gmail.com>
2022-02-06 01:37:49 +0100

    screenshot + ssh + scp + first step create image folder

---
+++ acquisition_identity/index.py
@@ -0,0 +1,58 @@
+import cv2
+import face_recognition
+import sys 
+import os
+import sys
+
+sys.path.append('../')
+
+#from services import ssh
+from services import ssh_scp as conn
+
+############### UPLOAD ##################
+
+#conn.scp_upload('./face_encodings.npy')
+#conn.scp_upload('./face_names.npy')
+
+############### DOWNLOAD ##################
+
+#conn.scp_download('mirori_faces/*', "./")
+#print(conn.ssh_command("ls -l mirori_faces/"))
+
+##################################################################
+
+video_capture = cv2.VideoCapture(0)
+
+face_locations = []
+
+ID=1 #API CALL 
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()  # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]  # Find all the faces in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)  # Display the results
+    for top, right, bottom, left in face_locations:
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)  # Display the resulting image
+        cv2.imshow('Video', frame)
+        
+    key = cv2.waitKey(1)
+    if key & key % 256 == 27:
+        # ESC pressed for Quit
+        video_capture.release()
+        cv2.destroyAllWindows()
+        break
+    elif key & key % 256 == 32:
+        # SPACE pressed for Screenshot
+        img_name = "identity.png"
+        parent_folder = "../identified/"
+        folder = ID
+        path = os.path.join(parent_folder, str(folder))
+        os.mkdir(path, 0o777)
+        print(path + img_name)
+        cv2.imwrite(path + img_name, frame)
+        video_capture.release()
+        cv2.destroyAllWindows()
+        ID+=1
+        print(format(img_name))
+        break
---
+++ acquisition_identity/prepare_embedding.py
@@ -0,0 +1,57 @@
+import face_recognition
+import numpy as np
+import os
+import glob
+from npy_append_array import NpyAppendArray
+
+subprocess.check_output(['ssh', 'my_server', 'echo /*/'])
+
+dir_identites="../known_faces/"
+
+face_encodings=[]
+face_names=[]
+is_npy_file = os.path.isfile('face_names.npy')
+
+if is_npy_file:
+    npyFaceName = np.load('face_names.npy')
+    npyFaceEncode = np.load('face_encodings.npy')
+
+
+id=1
+for dir_identite in os.listdir(dir_identites):
+    print("ID", id)
+    id+=1
+
+    fichiers=[]
+    for ext in ["*.jpg", "*.jpeg", "*.png"]:
+        fichiers.extend(glob.glob(dir_identites+"/"+dir_identite+"/"+ext))
+    if len(fichiers)==0:
+        print("Repertoire vide", dir_identite)
+        continue
+    for fichier in fichiers:
+        filename = dir_identite.replace('_', ' ')
+        if is_npy_file and filename not in npyFaceName or not is_npy_file:
+            image=face_recognition.load_image_file(fichier)
+            embedding=face_recognition.face_encodings(image)[0]
+            face_encodings.append(embedding)
+            face_names.append(filename)
+
+if is_npy_file and len(face_names) > 0 :
+    for faceEncoding in face_encodings:            
+        np.save("face_encodings", np.array(np.append(npyFaceEncode, faceEncoding)))
+        print('Append encoding new face')
+    print(face_names)
+    for faceName in face_names: 
+        np.save("face_names", np.array(np.append(npyFaceName, faceName)))
+        print('Append new face', faceName, '\n')
+    print('New array', np.load('face_names.npy'))
+         
+elif len(face_names) > 0 :
+    np.save("face_encodings", np.array(face_encodings))
+    print('encoding all faces', embedding)
+    np.save("face_names", np.array(face_names))
+    print('Add all face names', face_names) 
+
+else:
+    print("Total ID's:", id)
+    print('Nothing to add')
